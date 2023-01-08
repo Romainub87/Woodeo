30d339f84f5880e1892ddf46205e79b1d77e5895
valentin <valentin.raillard@etu.u-bordeaux.fr>
2023-01-08 23:32:11 +0100

    Inversion d'un if pour que le code soit plus lisible

--- symfony/src/Controller/AdminController.php
+++ symfony/src/Controller/AdminController.php
@@ -15,31 +15,31 @@ class AdminController extends AbstractController
     #[Route('/admin_dashboard', name: 'app_admin_dashboard')]
     public function index(Request $request, ManagerRegistry $doctrine): Response
     {
-        if ($this->getUser()->isAdmin()) {
-            $form = $this->createFormBuilder()
-                ->add('email')
-                ->getForm();
-            $form->handleRequest($request);
-            
-            if ($form->isSubmitted() && $form->isValid()) {
-                $user = $doctrine->getRepository(User::class)->findOneBy(array('email' => $form->get('email')->getData()));
-                if ($user == null) {
-                    $form->addError(new FormError('Utilisateur introuvable'));
-                } else {
-                    $user->setAdmin(true);
-                    $entityManager = $doctrine->getManager();
-                    $entityManager->persist($user);
-                    $entityManager->flush();
-                    return $this->redirectToRoute('app_admin_dashboard');
-                }
-            }
-
-            return $this->render('admin/index.html.twig', [
-                'controller_name' => 'AdminController',
-                'form' => $form->createView(),
-            ]);
-        } else {
+        if (!$this->getUser() || !$this->getUser()->isAdmin()) {
             return $this->redirectToRoute('app_series_index');
         }
+
+        $form = $this->createFormBuilder()
+            ->add('email')
+            ->getForm();
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $doctrine->getRepository(User::class)->findOneBy(array('email' => $form->get('email')->getData()));
+            if ($user == null) {
+                $form->addError(new FormError('Utilisateur introuvable'));
+            } else {
+                $user->setAdmin(true);
+                $entityManager = $doctrine->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+                return $this->redirectToRoute('app_admin_dashboard');
+            }
+        }
+
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+            'form' => $form->createView(),
+        ]);
     }
 }
