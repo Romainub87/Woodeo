5facf3b8d58ae8b8ba13bc5e4ae387ec9c588dd4
valentin <valentin.raillard@etu.u-bordeaux.fr>
2023-01-08 23:03:17 +0100

    AdminController gère maintenant la promotion d'un utilisateur

--- symfony/src/Controller/AdminController.php
+++ symfony/src/Controller/AdminController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Form\FormError;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin_dashboard', name: 'app_admin_dashboard')]
-    public function index(): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
         if ($this->getUser()->isAdmin()) {
+            $form = $this->createFormBuilder()
+                ->add('email')
+                ->getForm();
+            $form->handleRequest($request);
+            
+            if ($form->isSubmitted() && $form->isValid()) {
+                $user = $doctrine->getRepository(User::class)->findOneBy(array('email' => $form->get('email')->getData()));
+                if ($user == null) {
+                    $form->addError(new FormError('Utilisateur introuvable'));
+                } else {
+                    $user->setAdmin(true);
+                    $entityManager = $doctrine->getManager();
+                    $entityManager->persist($user);
+                    $entityManager->flush();
+                    return $this->redirectToRoute('app_admin_dashboard');
+                }
+            }
+
             return $this->render('admin/index.html.twig', [
                 'controller_name' => 'AdminController',
+                'form' => $form->createView(),
             ]);
         } else {
             return $this->redirectToRoute('app_series_index');
