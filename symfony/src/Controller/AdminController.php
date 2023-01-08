0e1b038f3ec03036fe9b73c09c327274cb759322
valentin <valentin.raillard@etu.u-bordeaux.fr>
2023-01-08 21:42:50 +0100

    Ajout du controller Admin(sécurisé) et bouton dashboard(pour admin)

---
+++ symfony/src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin_dashboard', name: 'app_admin_dashboard')]
+    public function index(): Response
+    {
+        if ($this->getUser()->isAdmin()) {
+            return $this->render('admin/index.html.twig', [
+                'controller_name' => 'AdminController',
+            ]);
+        } else {
+            return $this->redirectToRoute('app_series_index');
+        }
+    }
+}
