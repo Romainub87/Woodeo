08b355a7e8123a256ba89458830fecd699a73774
valentin <valentin.raillard@etu.u-bordeaux.fr>
2023-01-09 17:57:48 +0100

    L'email dans le register doit maintenant être valide pour se register

--- symfony/src/Form/RegistrationFormType.php
+++ symfony/src/Form/RegistrationFormType.php
@@ -11,13 +11,14 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class)
             ->add('name')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
