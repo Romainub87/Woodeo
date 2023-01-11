d555e3c0872ea886bde00370d635b0a4a281e894
valentin <valentin.raillard@etu.u-bordeaux.fr>
2023-01-11 11:22:56 +0100

    Affichage des séries aléatoires + optimisation des requêtes

--- symfony/src/Controller/SeriesController.php
+++ symfony/src/Controller/SeriesController.php
@@ -3,16 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Series;
-use App\Form\SeriesType;
-use App\Entity\User;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Component\Pager\PaginatorInterface;
-use Doctrine\ORM\EntityRepository;
 
 #[Route('/series')]
 class SeriesController extends AbstractController
@@ -22,41 +18,34 @@ class SeriesController extends AbstractController
     {
         $series = $entityManager
             ->getRepository(Series::class)
-            ->findBy(array(), array('title' => 'ASC'));
-        
-        $seriesRandom=$series;
-        
+            ->createQueryBuilder('s')
+            ->orderBy('s.title', 'ASC');
+
         $liste_series = $paginator->paginate(
             $series,
             $request->query->getInt('page', 1),
             8
         );
-
-        /*Boucle pour les 8 premiers de $series*/
-        $tmp = false;
-        while(!$tmp){
-            $tmp = true;
-            #random series
-            shuffle($seriesRandom);
-            for($i=0;$i<8;$i++){
-                for($j=0;$j<10;$j++){
-                    if($liste_series[$i]->getId() == $seriesRandom[$j]->getId()){
-                        $tmp = false;
-                    }
-                }
-            }
-        }
-
-        $liste_seriesRandom = $paginator->paginate(
-            $seriesRandom,
-            $request->query->getInt('page', 1),
-            10
-        );
                 
-
         return $this->render('series/index.html.twig', [
             'series' => $liste_series,
-            'seriesRandom' => $liste_seriesRandom,
+        ]);
+    }
+
+    #[Route('/random', name: 'app_series_random')]
+    public function random(Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
+    {
+        $series = $entityManager
+            ->getConnection()
+            ->query('SELECT id, title, poster FROM series ORDER BY RAND() LIMIT 10')
+            ->fetchAllAssociative();
+        
+        foreach ($series as &$serie) {
+            $serie['poster'] = "data:image/png;base64,".base64_encode($serie['poster']);
+        };
+        
+        return $this->render('series/random.html.twig', [
+            'series' => $series,
         ]);
     }
 
@@ -81,51 +70,5 @@ class SeriesController extends AbstractController
 
         return $this->redirectToRoute('app_series_show', ['id'=>$series->getId()], Response::HTTP_SEE_OTHER);
     }
-
-
-    /* TEST ------------------------------------------------------- */
-    /*
-    #[Route('/random', name: 'app_series_index', methods: ['GET'])]
-    public function random(Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
-    {
-        $series = $entityManager
-            ->getRepository(Series::class)
-            ->findBy(array(), array('title' => 'ASC'));
-        
-        $seriesRandom=$series;
-        
-        $liste_series = $paginator->paginate(
-            $series,
-            $request->query->getInt('page', 1),
-            8
-        );
-
-        #Boucle pour les 8 premiers de $series
-        $tmp = false;
-        while(!$tmp){
-            $tmp = true;
-            #random series
-            shuffle($seriesRandom);
-            for($i=0;$i<8;$i++){
-                for($j=0;$j<10;$j++){
-                    if($liste_series[$i]->getId() == $seriesRandom[$j]->getId()){
-                        $tmp = false;
-                    }
-                }
-            }
-        }
-
-        $liste_seriesRandom = $paginator->paginate(
-            $seriesRandom,
-            $request->query->getInt('page', 1),
-            10
-        );
-                
-
-        return $this->render('series/random.html.twig', [
-            'series' => $liste_series,
-            'seriesRandom' => $liste_seriesRandom,
-        ]);
-    }
-    */
+    
 }
