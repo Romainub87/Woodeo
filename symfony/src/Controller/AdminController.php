bbae2913dcee5cc5c3240543e5724d20855b71ca
Raillard Valentin <valentin.raillard@etu.u-bordeaux.fr>
2023-01-16 15:23:51 +0100

    Page search IMDb + mise à jours des liens

--- symfony/src/Controller/AdminController.php
+++ symfony/src/Controller/AdminController.php
@@ -7,16 +7,32 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin_dashboard', name: 'app_admin_dashboard')]
-    public function index(Request $request, ManagerRegistry $doctrine): Response
+    public function index(Request $request, ManagerRegistry $doctrine, HttpClientInterface $client): Response
     {
-        return $this->redirectToRoute('app_user_index');
+        $form = $this->createFormBuilder()
+            ->add('title', TextType::class)
+            ->getForm();
 
-    //     return $this->render('admin/index.html.twig', [
-    //         'controller_name' => 'AdminController',
-    //     ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $response = $client->request('GET', 'http://www.omdbapi.com/?t='.$form->get('title')->getData().'&apikey=a2996c2f&type=series');
+            return $this->render('admin/index.html.twig', [
+                'response' => $response->toArray(),
+                'form' => $form->createView(),  
+            ]);
+        }
+        
+
+        return $this->render('admin/index.html.twig', [
+            'form' => $form->createView(),  
+            'response' => '',
+        ]);
     }
 }
