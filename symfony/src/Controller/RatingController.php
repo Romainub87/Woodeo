5106ae42ae571a6a2c534b372b7fee0c92e6f511
rlamat <remi.lamat@etu.u-bordeaux.fr>
2023-01-17 08:31:16 +0100

    modif

--- symfony/src/Controller/RatingController.php
+++ symfony/src/Controller/RatingController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Series;
+use App\Entity\User;
 
 #[Route('/rating')]
 class RatingController extends AbstractController
